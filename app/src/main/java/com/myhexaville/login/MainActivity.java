0f7680587a491506091861fb734edbd6cee0d161
IhorKlimov <iklimov6@gmail.com>
2017-04-18 21:56:30 +0200

    fragment change looks fince

--- app/src/main/java/com/myhexaville/login/MainActivity.java
+++ app/src/main/java/com/myhexaville/login/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
 
 import com.myhexaville.login.databinding.ActivityMainBinding;
 import com.myhexaville.login.login.LoginFragment;
@@ -15,7 +14,6 @@ import com.myhexaville.login.login.SignUpFragment;
 
 
 public class MainActivity extends AppCompatActivity {
-
     private ActivityMainBinding binding;
     private boolean isLogin = true;
 
@@ -25,11 +23,14 @@ public class MainActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.bottom, new SignUpFragment())
-                .replace(R.id.top, new LoginFragment())
+                .replace(R.id.bottom_login, new LoginFragment())
+                .replace(R.id.top_login, new LoginFragment())
+                .replace(R.id.bottom_sign_up, new SignUpFragment())
+                .replace(R.id.top_sign_up, new SignUpFragment())
                 .commit();
 
-        binding.top.setRotation(-90);
+        binding.topLogin.setRotation(-90);
+        binding.topSignUp.setRotation(90);
 
         binding.button.setOnButtonSwitched(isLogin ->
                 binding.getRoot()
@@ -37,46 +38,42 @@ public class MainActivity extends AppCompatActivity {
                                 this,
                                 isLogin ? R.color.colorPrimary : R.color.colorAccent)));
 
+        binding.bottomLogin.setAlpha(0f);
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        binding.top.setPivotX(binding.top.getWidth() / 2);
-        binding.top.setPivotY(binding.top.getHeight());
+        binding.topLogin.setPivotX(binding.topLogin.getWidth() / 2);
+        binding.topLogin.setPivotY(binding.topLogin.getHeight());
+        binding.topSignUp.setPivotX(binding.topLogin.getWidth() / 2);
+        binding.topSignUp.setPivotY(binding.topLogin.getHeight());
     }
 
-    // todo fix ugly replacement
     public void rotate(View view) {
-        binding.top.animate().rotation(0).setListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                if (isLogin) {
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.bottom, new LoginFragment())
-                            .replace(R.id.top, new SignUpFragment())
-                            .commit();
-                    binding.top.setRotation(90);
-                } else {
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.bottom, new SignUpFragment())
-                            .replace(R.id.top, new LoginFragment())
-                            .commit();
-                    binding.top.setRotation(-90);
+        if (isLogin) {
+            binding.topLogin.animate().rotation(0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    binding.bottomLogin.setAlpha(1f);
+                    binding.topLogin.setRotation(-90);
                 }
+            });
+            binding.bottomSignUp.animate().alpha(0f);
+        } else {
+            binding.topSignUp.animate().rotation(0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    binding.bottomSignUp.setAlpha(1f);
+                    binding.topSignUp.setRotation(90);
+                }
+            });
+            binding.bottomLogin.animate().alpha(0f);
+        }
 
-                isLogin = !isLogin;
-            }
-        });
-
-        binding.bottom.animate().alpha(0f).setListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                binding.bottom.setAlpha(1f);
-            }
-        });
+        isLogin = !isLogin;
         binding.button.startAnimation();
     }
 
