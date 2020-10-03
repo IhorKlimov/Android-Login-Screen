ba4f088d9706489a68a6c60008e154f2bcd97f38
AnushkaSharma503 <59316156+AnushkaSharma503@users.noreply.github.com>
2020-10-03 14:30:26 +0530

    Update MainActivity.java
    Add one line code to create full screen on the activity .

--- app/src/main/java/com/myhexaville/login/MainActivity.java
+++ app/src/main/java/com/myhexaville/login/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // create full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         LoginFragment topLoginFragment = new LoginFragment();
